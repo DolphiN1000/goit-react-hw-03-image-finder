fc03ea8bf6be6f363ff213d500ae8b6ec4e6a16b
DolphiN1000 <88268032+DolphiN1000@users.noreply.github.com>
2023-02-26 00:00:57 +0200

    fix imageGallery

--- src/modules/ImageGallery/ImageGallery.jsx
+++ src/modules/ImageGallery/ImageGallery.jsx
@@ -1,24 +1,24 @@
 // import PropTypes from 'prop-types';
 import ImageGalleryItem from './ImageGalleryItem/ImageGalleryItem';
 
+import styles from './imageGallery.module.scss';
+
 // import styles from './imageGallery.module.scss';
 
 const ImageGallery = ({ items, showImage }) => {
-  const elements = items.map(
-    ({ id, webformatURL, largeImageURL, tags }) => {
-      return (
-        <ImageGalleryItem
-          showImage={showImage}
-          key={id}
-          webformatURL={webformatURL}
-          largeImageURL={largeImageURL}
-          tags={tags}
-        />
-      );
-    }
-  );
+  const elements = items.map(({ id, webformatURL, largeImageURL, tags }) => {
+    return (
+      <ImageGalleryItem
+        showImage={showImage}
+        key={id}
+        webformatURL={webformatURL}
+        largeImageURL={largeImageURL}
+        tags={tags}
+      />
+    );
+  });
 
-  return <ul >{elements}</ul>;
+  return <ul className={styles.gallery}>{elements}</ul>;
 };
 
 export default ImageGallery;
